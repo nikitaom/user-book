927a6180479e2f7986d7445f00ad1bcd37e8b346
Nikita Omelyanyuk <nikitaom12344321@gmail.com>
2018-09-19 16:48:45 +0300

    working controller

--- src/main/java/main/controllers/UserController.java
+++ src/main/java/main/controllers/UserController.java
@@ -1,7 +1,8 @@
 package main.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+@Controller
 public class UserController {
     @GetMapping("/q")
     public String getUsers() {
